d6cbc1e5e40742a230c07f7b9c21c9aaaec82748
Riyu2001 <bhardwajriya2001@gmail.com>
2024-02-22 12:47:29 +0530

    erhe

---
+++ largestelement.cpp
@@ -0,0 +1,28 @@
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"enter size of array : ";
+    cin>>n;
+    int arr[n];
+    int user_input;
+    int largest;
+
+    for(int i=0; i<n;i++){
+       
+        cout<<"enter element : ";
+        arr[i]=user_input;
+        cin>>user_input;
+    }
+    largest = arr[0];
+    for(int j=0; j<n ;j++){
+        
+        if(arr[j] > largest){
+        largest= arr[j];
+        } 
+    } 
+    cout<<"largest element of array is : "<<largest<<endl;
+}
